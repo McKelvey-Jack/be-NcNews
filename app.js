448a946553d7f179afb50e4d3f7129515694dfaf
Jack McKelvey <71670664+McKelvey-Jack@users.noreply.github.com>
2020-12-07 12:36:35 +0000

    adding cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routers/api-router');
+const cors = require('cors');
 const {
   internalErrorHandler,
   customErrorHandler,
   send404,
   PSQLerrors,
 } = require('./errors');
+
+app.use(cors());
 
 app.use(express.json());
 
